55dd7fe8a34f4efc2ee3f6a584db2ce203142991
rb-agency <rb.for.marketing@gmail.com>
2024-04-14 12:22:02 +0200

    first commit

---
+++ resources/views/includes/tinyeditor.blade.php
@@ -0,0 +1,17 @@
+<script src="https://cdn.tiny.cloud/1/20it1443jt0f9xsg3iss831wgqdc6m8008srowelr8n0bwpp/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+<script>
+    var editor_config = {
+        path_absolute : "/",
+        selector: "textarea",
+        plugins: [
+            "advlist autolink lists link charmap print preview hr anchor pagebreak",
+            "searchreplace wordcount visualblocks visualchars code fullscreen",
+            "insertdatetime nonbreaking save table contextmenu directionality",
+            "emoticons template paste textcolor colorpicker textpattern"
+        ],
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+        relative_urls: false,
+    };
+
+    tinymce.init(editor_config);
+</script>
